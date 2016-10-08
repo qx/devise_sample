dd55239f2f65c0ac26285b8bff0000ea67d084d6
oyqx17 <44708869@qq.com>
2016-10-08 10:29:13 +0800

    add login/out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,21 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.ym.163.com",
+      :port => "25",
+      :domain => "it-place.org",
+      :authentication => "plain",
+      :user_name => "test_sender@it-place.org",
+      :password => "test_sender",
+      :enable_starttls_auto => true
+  }
 end
