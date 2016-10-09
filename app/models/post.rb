1af650cab3d9873358a07359820bcb6c33b92e66
oyqx17 <44708869@qq.com>
2016-10-09 20:25:09 +0800

    Completed:   test markable To do:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Post < ActiveRecord::Base
+ validates :user_id, presence: true
+ belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
